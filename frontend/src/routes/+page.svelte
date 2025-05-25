7255ec19718e0e88c12e76813ec298b186c29fd6
AnuragVish1 <anuragvishwakarmaalt@gmail.com>
2025-05-25 18:55:54 +0530

    refact

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div class=" font-rubik h-screen w-full items-center justify-center flex flex-col gap-7 text-xl">
+	<h1 class=" ">Welcome to SvelteKit</h1>
+	<p>
+		Visit <a class=" text-[#e42f2f] border-b-2" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+</div>
